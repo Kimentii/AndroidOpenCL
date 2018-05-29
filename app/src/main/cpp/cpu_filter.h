80ce806da2f3c3a8cfc0dcf7c220bc8d643244c5
zoxal <zzoxall@gmail.com>
2018-05-29 14:35:48 +0300

    repo cleanup

--- app/src/main/cpp/cpu_filter.h
+++
@@ -1,9 +0,0 @@
-#ifndef CPU_FILTER_H
-#define CPU_FILTER_H
-
-#include "ppm.h"
-#include "filter.h"
-
-unsigned char* filter_via_cpu(const PPMImage* image, ftype* filter, const int filter_size);
-
-#endif
--- app/src/main/cpp/time_measure.h
+++
@@ -1,9 +0,0 @@
-#ifndef TIME_MEASURE_H
-#define TIME_MEASURE_H
-
-#include <stdint.h>
-
-void time_snap();
-uint64_t time_elapsed();
-
-#endif
